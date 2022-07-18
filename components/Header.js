34d6585ebfe7103d99a0905f64f43f241a64f9f6
y_develop <65386960+yhoi@users.noreply.github.com>
2022-07-18 20:26:00 +0900

    feature/簡易的なHeader実装

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import { Box, Text, Divider, Grid, Image } from "@chakra-ui/react";
+export default function Header() {
+  return (
+    <>
+      <Box bg="#353535" padding={8}>
+        <Image src="img/header_icon.png" />
+      </Box>
+    </>
+  );
+}
